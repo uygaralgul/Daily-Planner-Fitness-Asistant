e42e8233a3d81be8d6790416cc0207e5868f39b6
uygaralgul <alguluygar@gmail.com>
2026-04-06 16:27:01 +0300

    Delete DailyPlanner directory

--- DailyPlanner/Models/UserProfile.cs
+++
@@ -1,8 +0,0 @@
-namespace DailyPlanner.Models
-{
-    public class UserProfile
-    {
-        public string Name { get; set; } = "Kullanıcı";
-        public string AvatarPath { get; set; } = null;
-    }
-}
